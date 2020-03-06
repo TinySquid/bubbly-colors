0e0ac2b7b5c4f9364c843238e56a57a96231b861
Michael Nunes <tinysquid.git@gmail.com>
2020-03-05 21:44:04 -0800

    Refactor dbConfig file

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,7 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
+require("dotenv").config();
 
-module.exports = knex(knexConfig.development);
+const knex = require("knex");
+const config = require("../knexfile.js");
+const environment = process.env.DB_ENV || "development";
+
+module.exports = knex(config[environment]);
