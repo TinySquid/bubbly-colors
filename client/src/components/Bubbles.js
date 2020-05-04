5a711c1b1a662d73f9e24e5a1f9291232c7f9e59
Michael Nunes <tinysquid.git@gmail.com>
2020-05-04 13:58:05 -0700

    Add accessibility modifications to emojis

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -3,42 +3,50 @@ import { Pack } from "@potion/layout";
 import { Svg, Circle } from "@potion/element";
 
 const Bubbles = ({ colors, refresh }) => {
-  const [bubbleData, setBubbleData] = useState([]);
-  useEffect(() => {
-    const generateBubbleData = colors.map((_, i) => ({
-      value: Math.floor(Math.random() * (colors.length * 2)) + 1,
-      key: `${i + 2}`,
-    }));
-    setBubbleData(generateBubbleData);
-  }, [colors, refresh]);
+    const [bubbleData, setBubbleData] = useState([]);
+    useEffect(() => {
+        const generateBubbleData = colors.map((_, i) => ({
+            value: Math.floor(Math.random() * (colors.length * 2)) + 1,
+            key: `${i + 2}`,
+        }));
+        setBubbleData(generateBubbleData);
+    }, [colors, refresh]);
 
-  return (
-    <div className="bubble-wrap">
-      <p>😃 bubbles 😃</p>
-      <Svg width={400} height={400}>
-        <Pack
-          data={{
-            children: bubbleData,
-          }}
-          sum={(datum) => datum.value}
-          size={[400, 400]}
-          includeRoot={false}
-          nodeEnter={(d) => ({ ...d, r: 0 })}
-          animate>
-          {(nodes) =>
-            nodes
-              .map(({ x, y, r, key }, i) => {
-                if (i < colors.length) {
-                  return <Circle key={key} cx={x} cy={y} r={r} fill={colors[i].hex} />;
-                }
-                return null;
-              })
-              .filter((v) => v)
-          }
-        </Pack>
-      </Svg>
-    </div>
-  );
+    return (
+        <div className="bubble-wrap">
+            <p>
+                <span role="img" aria-label="smiley emoji">
+                    😃
+                </span>{" "}
+                bubbles{" "}
+                <span role="img" aria-label="smiley emoji">
+                    😃
+                </span>
+            </p>
+            <Svg width={400} height={400}>
+                <Pack
+                    data={{
+                        children: bubbleData,
+                    }}
+                    sum={(datum) => datum.value}
+                    size={[400, 400]}
+                    includeRoot={false}
+                    nodeEnter={(d) => ({ ...d, r: 0 })}
+                    animate>
+                    {(nodes) =>
+                        nodes
+                            .map(({ x, y, r, key }, i) => {
+                                if (i < colors.length) {
+                                    return <Circle key={key} cx={x} cy={y} r={r} fill={colors[i].hex} />;
+                                }
+                                return null;
+                            })
+                            .filter((v) => v)
+                    }
+                </Pack>
+            </Svg>
+        </div>
+    );
 };
 
 export default Bubbles;
